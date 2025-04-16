974f3e3243d40b57632e04df30ac1b90d07379a3
emilyrh <114513651+emilyrh@users.noreply.github.com>
2025-04-16 16:34:36 +0100

    Improved link

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      This project was coded by{" "}
-      <a href="http://" target="_blank">
-        Emily
+      This project was coded by Emily Huggins and is{" "}
+      <a href="https://subtle-strudel-f1c2d7.netlify.app/" target="_blank">
+        open-sourced
       </a>
     </footer>
   );
